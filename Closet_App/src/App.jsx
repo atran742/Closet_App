2bda3b6141651f31d551dd9e6f4702059043abaf
atran742 <atran3420@sdsu.edu>
2026-09-16 15:05:54 -0700

    Configure GitHub Pages deployment

--- Closet_App/src/App.jsx
+++ Closet_App/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import ClosetGrid from "./ClosetGrid";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<ClosetGrid />} />
         <Route path="/generate" element={<MainPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
